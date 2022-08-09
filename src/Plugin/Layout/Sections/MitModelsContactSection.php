2504332e1e367b9278ba5a2ae06e7128db61db16
noumecha spaker <noumechaivan@gmail.com>
2022-08-09 13:55:33 +0100

    maj: tmc

--- src/Plugin/Layout/Sections/MitModelsContactSection.php
+++ src/Plugin/Layout/Sections/MitModelsContactSection.php
@@ -66,15 +66,6 @@ class MitModelsContactSection extends FormatageModelsSection {
         return [
             'load_libray' => true,
             'region_tag_title' => 'h1',
-            "derivate" => [
-                'value' => 'right',
-                'options' => [
-                    'right' => 'right',
-                    'left' => 'left',
-                    'right_cover' => 'right__cover',
-                    'left_cover' => 'left__cover'
-                ]
-            ],
             'infos' => [
                 'builder-form' => true,
                 'info' => [
@@ -97,7 +88,24 @@ class MitModelsContactSection extends FormatageModelsSection {
                     'contact_list' => [
                         'text_html' => [
                             'label' => ' Contact List Item ',
-                            'value' => '  '
+                            'value' => '<li class="list-element">
+                                            <span class="icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="35" height="35" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13s13-5.8 13-13c0-1.4-.188-2.794-.688-4.094L26.688 13.5c.2.8.313 1.6.313 2.5c0 6.1-4.9 11-11 11S5 22.1 5 16S9.9 5 16 5c3 0 5.694 1.194 7.594 3.094L25 6.688C22.7 4.388 19.5 3 16 3zm11.28 4.28L16 18.563l-4.28-4.28l-1.44 1.437l5 5l.72.686l.72-.687l12-12l-1.44-1.44z"/></svg>
+                                            </span>
+                                            <p class="content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda mollitia officiis tempora, molestiae ut unde!</p>
+                                        </li>
+                                        <li class="list-element">
+                                            <span class="icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="35" height="35" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13s13-5.8 13-13c0-1.4-.188-2.794-.688-4.094L26.688 13.5c.2.8.313 1.6.313 2.5c0 6.1-4.9 11-11 11S5 22.1 5 16S9.9 5 16 5c3 0 5.694 1.194 7.594 3.094L25 6.688C22.7 4.388 19.5 3 16 3zm11.28 4.28L16 18.563l-4.28-4.28l-1.44 1.437l5 5l.72.686l.72-.687l12-12l-1.44-1.44z"/></svg>
+                                            </span>
+                                            <p class="content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda mollitia officiis tempora, molestiae ut unde!</p>
+                                        </li>
+                                        <li class="list-element">
+                                            <span class="icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="35" height="35" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13s13-5.8 13-13c0-1.4-.188-2.794-.688-4.094L26.688 13.5c.2.8.313 1.6.313 2.5c0 6.1-4.9 11-11 11S5 22.1 5 16S9.9 5 16 5c3 0 5.694 1.194 7.594 3.094L25 6.688C22.7 4.388 19.5 3 16 3zm11.28 4.28L16 18.563l-4.28-4.28l-1.44 1.437l5 5l.72.686l.72-.687l12-12l-1.44-1.44z"/></svg>
+                                            </span>
+                                            <p class="content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda mollitia officiis tempora, molestiae ut unde!</p>
+                                        </li>  '
                         ]
                     ],
                     'contact_link' => [
@@ -106,14 +114,14 @@ class MitModelsContactSection extends FormatageModelsSection {
                             "value" => [
                                 "link" => "#",
                                 "text" => "Contact Us",
-                                "class" => "btn btn-outline-primary"
+                                "class" => "link-us"
                             ]
                         ]
                     ],
                     'contact_image' => [
-                        'img_bg' => [
+                        'text_html' => [
                             'label' => 'image',
-                            'fids' => []
+                            'value' => '<img src="http://slidesigma.com/themes/html/mitor/assets/img/homepage-1/product-2.jpg" class="contact-lutin" alt="">'
                         ]
                     ]
                 ]
