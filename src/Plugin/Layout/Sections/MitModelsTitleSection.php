8c2729c626ba9461f648b8ec7aee70d74a488878
noumecha spaker <noumechaivan@gmail.com>
2022-08-10 11:54:58 +0100

    maj : define title section

---
+++ src/Plugin/Layout/Sections/MitModelsTitleSection.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Drupal\mit_models\Plugin\Layout\Sections;
+
+use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
+use Drupal\formatage_models\FormatageModelsThemes;
+use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
+
+/**
+ * A contact section layout from mit_models
+ *
+ * @Layout(
+ *   id = "mit_models_title_section",
+ *   label = @Translation(" Mit_Models : Title "),
+ *   category = @Translation("mit_models"),
+ *   path = "layouts/sections",
+ *   template = "mit_models_title_section",
+ *   library = "mit_models/mit_models_title_section",
+ *   default_region = "title",
+ *   regions = {
+ *     "title" = {
+ *       "label" = @Translation("title"),
+ *     },
+ *     "description" = {
+ *       "label" = @Translation("description"),
+ *     },
+ *   }
+ * )
+ */
+
+class MitModelsTitleSection extends FormatageModelsSection {
+
+    /**
+     *
+     * {@inheritdoc}
+     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::__construct()
+     */
+    public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager) {
+        // TODO Auto-generated method stub
+        parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
+        $this->pluginDefinition->set('icon', drupal_get_path('module', 'mit_models') . "/icones/sections/mit_models_title.png");
+    }
+    
+    /**
+     *
+     * {@inheritdoc}
+     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::build()
+     */
+    public function build(array $regions) {
+        // TODO Auto-generated method stub
+        $build = parent::build($regions);
+        FormatageModelsThemes::formatSettingValues($build);
+        return $build;
+    }
+    
+    /**
+     * 
+     * {@inheritdoc}
+     * 
+     */
+    public function defaultConfiguration() {
+        return parent::defaultConfiguration() + [
+            'load_library' => true,
+            'region_tag_title' => 'h1',
+            'infos' => [
+                'builder_form' => true,
+                'info' => [
+                    'title' => 'Formulaire de contenu',
+                    'loader' => 'static',
+                ],
+                'fields' => [
+                    'title' => [
+                        'text_html' => [
+                            'title' => 'Titre',
+                            'value' => 'What Services We Offers'
+                        ]
+                    ],
+                    'description' => [
+                        'text_html' => [
+                            'title' => 'Description',
+                            'value' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate deleniti vero quaerat, eum voluptas
+                            odio asperiores unde molestiae dolorem enim, sunt facere soluta!'
+                        ]
+                    ]
+                ]
+            ]
+        ];
+    }
+
+}
