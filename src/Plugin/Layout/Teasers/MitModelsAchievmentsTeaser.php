1e04ff3a87cf42f514b9aa02d76de48f582fa3d6
noumecha spaker <noumechaivan@gmail.com>
2022-08-24 09:37:05 +0100

    maj: perform the achieve teaser

--- src/Plugin/Layout/Teasers/MitModelsAchievmentsTeaser.php
+++ src/Plugin/Layout/Teasers/MitModelsAchievmentsTeaser.php
@@ -24,6 +24,9 @@ use Drupal\formatage_models\Plugin\Layout\Teasers\FormatageModelsTeasers;
  *     "ach_sub_desc" = {
  *       "label" = @Translation("ach_sub_desc"),
  *     },
+ *     "ach_svg" = {
+ *       "label" = @Translation("ach_svg"),
+ *     },
  *   }
  * )
  */
@@ -76,6 +79,15 @@ class MitModelsAchievmentsTeaser extends FormatageModelsTeasers {
                         'text_html' => [
                             'label' => 'Description',
                             'value' => ' Total Users '
+                        ]
+                    ],
+                    'ach_svg' => [
+                        'text_html' => [
+                            'label' => 'SVG icon',
+                            'value' => '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                            <path
+                                                d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z" />
+                                        </svg>'
                         ]
                     ]
                 ]
